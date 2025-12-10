ac6294c2e244106c2a3839b56016953f28a826c5
Harry <wheeler_harry@icloud.com>
2025-12-10 19:45:45 +0000

    Implement username check and login validation
    Added functions to check username existence and validate login credentials.

--- database.py
+++ database.py
@@ -8,7 +8,25 @@ def save_data(data):
     with open("user_data.json", "w") as f:
         json.dump(data, f, indent=4)
 
-def save_user(profile):
+def username_exists(username):
     data = load_data()
-    data["users"].append(profile)
+    for user in data["users"]:
+        if user["username"] == username:
+            return True
+    return False
+
+def validate_login(username, password):
+    data = load_data()
+    for user in data["users"]:
+        if user["username"] == username and user["password"] == password:
+            return user  # Return the entire saved user object
+    return None
+
+def save_user(username, password, profile):
+    data = load_data()
+    data["users"].append({
+        "username": username,
+        "password": password,
+        "profile": profile
+    })
     save_data(data)
